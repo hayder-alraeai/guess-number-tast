b4d519025a58496664e71bd044a8ac7d0c5d9f24
hayder86 <hayder@andrepeat.io>
2021-10-17 11:32:54 +0200

    fas 2

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 win_number = 9
+amount_guesses = 5
 
 
 def play():
+    guess_counter = 0
+
     print("Welcome to our game. you need to guess the secret number")
     while True:
+        if guess_counter > 0 and not guess_counter == amount_guesses:
+            print(f"You have {amount_guesses - guess_counter} try left!")
+        elif guess_counter == amount_guesses:
+            lose = input(
+                f"You have used all your available try {amount_guesses} Type any key to try again or 'q' to quit"
+            )
+            if lose.lower() == "q":
+                break
+            else:
+                guess_counter = 0
         guessed_number = input("Enter a number: press 'q' to quit the game!")
         if not guessed_number.isdigit() and guessed_number.lower() == "q":
             break
@@ -15,6 +28,7 @@ def play():
             win = input("you win! Type any key to continue or 'q' to quit")
             if win.lower() == "q":
                 break
+        guess_counter = guess_counter + 1
 
 
 play()
